a43b8e9a43c0331313d54b07f945f1a4f5e87ef2
Chao Ma <super11040@gmail.com>
2019-08-23 16:47:21 -0700

    change oddsRatio input
    use SpatialPoints for oddsRatio input instead of data.frame.

--- R/oddsRatio.R
+++ R/oddsRatio.R
@@ -2,17 +2,12 @@ oddsRatio <- function(pdR, inputP){
   if(class(pdR) != "RasterLayer" & class(pdR) != "RasterStack" & class(pdR) != "RasterBrick"){
     stop("input probability density map (pdR) should be one of the following class: RasterLayer, RasterStack or RasterBrick")
   }
-  if(class(inputP) != "data.frame" & class(inputP) !="SpatialPoints" & class(inputP) != "SpatialPolygons"){
-    stop("inputP should be one of the following class: data.frame, SpatialPoints or SpatialPolygons")
+  if(class(inputP) !="SpatialPoints" & class(inputP) != "SpatialPolygons"){
+    stop("inputP should be SpatialPoints or SpatialPolygons")
   }
-
-  if(class(inputP) == "data.frame" | class(inputP) == "SpatialPoints"){
-    if(class(inputP) == "data.frame"){
-      n <- nrow(inputP)
-    }
-    if(class(inputP) == "SpatialPoints"){
-      n <- length(inputP)
-    }
+  
+  if(class(inputP) == "SpatialPoints"){
+    n <- length(inputP)
     extrVals <- extract(pdR, inputP)
     if(n == 1){
       result1 <- (extrVals/(1-extrVals))/(maxValue(pdR)/(1-maxValue(pdR)))
@@ -31,7 +26,7 @@ oddsRatio <- function(pdR, inputP){
     result <- list(oddsRatio = result1, ratioToMaxMin = result2)
     names(result) <- c("P1/P2_odds_ratio", "odds of a pixel to the odds of the max/min pixel")
   }
-
+  
   if(class(inputP) == "SpatialPolygons"){
     if(dimensions(inputP) != 2){
       stop("If inputP are SpatialPolygons, there should be SpatialPolygons with two polygons")
@@ -44,6 +39,6 @@ oddsRatio <- function(pdR, inputP){
     result <- list(oddsRatio = result1, polygonCellRatio = result2)
     names(result) <- c("P1/P2_odds_ratio", "ratio of numbers of cells in two polygons")
   }
-
+  
   return(result)
 }
