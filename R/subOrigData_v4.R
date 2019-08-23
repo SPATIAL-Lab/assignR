c8c33553ec8e609e192ba8a1f382dbe8fbb6d79f
Gabriel Bowen <gabe.bowen@utah.edu>
2019-08-23 12:23:41 -0600

    Update DB and subOrigData

--- R/subOrigData_v4.R
+++ R/subOrigData_v4.R
@@ -1,6 +1,51 @@
-subOrigData <- function(taxon = NULL, group = NULL, mask = NULL) {
+subOrigData <- function(marker = "d2H", taxon = NULL, group = NULL, reference = NULL, 
+                        age_code = NULL, mask = NULL) {
+  
   data("knownOrig")
   result <- NULL
+  
+  if(!is.null(taxon)){
+    if(!all(taxon %in% unique(knownOrig$Taxon))){
+      warning("One or more taxa not present in database")
+    }
+    result = knownOrig[knownOrig$Taxon %in% taxon,]
+  }
+  if(!is.null(group)){
+    if(!all(group %in% unique(knownOrig$Group))){
+      warning("One or more groups not present in database")
+    }
+    r = knownOrig[knownOrig$Group %in% group,]
+    if(is.null(result)){
+      result = r
+    }else{
+      result = result[result$ID %in% r$ID,] 
+    }
+  }
+  if(!is.null(reference)){
+    if(!all(reference %in% unique(knownOrig$Reference))){
+      warning("One or more references not present in database")
+    }
+    r = knownOrig[knownOrig$Reference %in% reference,]
+    if(is.null(result)){
+      result = r
+    }else{
+      result = result[result$ID %in% r$ID,] 
+    }
+  }
+  if(!is.null(age_code)){
+    if(!all(age_code %in% unique(knownOrig$Age_code))){
+      warning("One or more age codes not present in database")
+    }
+    r = knownOrig[knownOrig$Age_code %in% age_code,]
+    if(is.null(result)){
+      result = r
+    }else{
+      result = result[result$ID %in% r$ID,] 
+    }
+  }
+  
+  
+  
   if (!is.null(taxon)){
     if (all(taxon %in% unique(knownOrig$Taxon))) {
       result <- knownOrig[knownOrig$Taxon %in% taxon, ]
@@ -46,5 +91,5 @@ subOrigData <- function(taxon = NULL, group = NULL, mask = NULL) {
     points(result, col = "red", cex = 0.5)
   }
   cat(length(result[,1]),"data points are found\n")
-  return(result[,"d2H"])
+  return(result[,marker])
 }
