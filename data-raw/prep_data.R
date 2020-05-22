c0d381e553e232bbf83b694fa1e46ddb0dc41462
Gabriel Bowen <gabe.bowen@utah.edu>
2020-05-22 16:26:23 -0600

    Add standards data

--- data-raw/prep_data.R
+++ data-raw/prep_data.R
@@ -20,7 +20,9 @@ isSymmetric(oam)
 
 #Standards definitions files
 hsds = read.csv("data-raw/H_stds.csv")
+hsds = hsds[, 1:17]
 osds = read.csv("data-raw/O_stds.csv")
+osds = osds[, 1:17]
 #Verify rownumber matches adjacency matrix dimensions
 nrow(hsds) == nrow(ham)
 nrow(osds) == nrow(oam)
@@ -31,9 +33,10 @@ all(row.names(oam) %in% osds$Scale)
 #Known origin data table
 knownOrig
 #Convert to SPDF
-  
+
   
 #Known origin projects table
 projects 
   
-#
+#Write it all to /data/
+use_data(ham, oam, hsds, osds, overwrite = TRUE)
