b340c3c6aa055fd31a919bbf84320c0b3864e9e9
Gabriel Bowen <gabe.bowen@utah.edu>
2020-06-22 09:03:42 -0600

    New knownOrig data prep

--- data-raw/prep_data.R
+++ data-raw/prep_data.R
@@ -1,42 +1,66 @@
 options(stringsAsFactors = FALSE)
+library(openxlsx)
+library(sp)
+library(devtools)
 
 #World outline map
 load("data-raw/wrld_simpl.rda")
+
+#grap WGS84 projection
+p = proj4string(wrld_simpl)
 
 #This one is internal
 use_data(wrld_simpl, internal = TRUE)
 
 #Standards adjacency matrix for H
-ham = read.csv("data-raw/H_connections.csv", row.names = 1)
+ham = read.xlsx("data-raw/ham.xlsx", rowNames = TRUE)
 ham = as.matrix(ham)
 #Verify matrix symmetry
 isSymmetric(ham)
 
 #Standards adjacency matrix for O
-oam = read.csv("data-raw/O_connections.csv", row.names = 1)
+oam = read.xlsx("data-raw/oam.xlsx", rowNames = TRUE)
 oam = as.matrix(oam)
 #Verify matrix xymmetry
 isSymmetric(oam)
 
 #Standards definitions files
-hsds = read.csv("data-raw/H_stds.csv")
-hsds = hsds[, 1:17]
-osds = read.csv("data-raw/O_stds.csv")
-osds = osds[, 1:17]
+hsds = read.xlsx("data-raw/hsds.xlsx")
+osds = read.xlsx("data-raw/osds.xlsx")
+
 #Verify rownumber matches adjacency matrix dimensions
 nrow(hsds) == nrow(ham)
 nrow(osds) == nrow(oam)
+
 #Verify that all matrix entries have a match in definition file
 all(row.names(ham) %in% hsds$Scale)
 all(row.names(oam) %in% osds$Scale)
 
 #Known origin data table
-knownOrig
-#Convert to SPDF
+knownOrig_sources = read.xlsx("data-raw/knownOrigNew.xlsx", 
+                              sheet = "knownOrig_sources")
+sites = read.xlsx("data-raw/knownOrigNew.xlsx", 
+                              sheet = "knownOrig_sites")
+knownOrig_samples = read.xlsx("data-raw/knownOrigNew.xlsx", 
+                              sheet = "knownOrig_samples")
 
-  
-#Known origin projects table
-projects 
+#check standard scale names
+ss = unique(knownOrig_sources$H_std_scale)
+ss = ss[!is.na(ss)]
+all(ss %in% hsds$Scale)
+ss = unique(knownOrig_sources$O_std_scale)
+ss = ss[!is.na(ss)]
+all(ss %in% osds$Scale)
+
+#check linking fields
+all(knownOrig_samples$Site_ID %in% sites$Site_ID)
+all(knownOrig_samples$Dataset_ID %in% knownOrig_sources$Dataset_ID)
+
+#Convert to SPDF
+knownOrig_sites = SpatialPointsDataFrame(sites[,2:3], 
+                                         data = sites[,c(1,4:ncol(sites))],
+                                         proj4string = CRS(p))
   
 #Write it all to /data/
-use_data(ham, oam, hsds, osds, overwrite = TRUE)
+use_data(ham, oam, hsds, osds, knownOrig_samples, knownOrig_sites, 
+          knownOrig_sources, overwrite = TRUE)
