5692bbb7279e86142858054f8df83ae855bf12ca
Gabriel Bowen <gabe.bowen@utah.edu>
2020-08-04 09:49:22 -0600

    Fix tests and examples

--- tests/testthat/test_subOrigData.R
+++ tests/testthat/test_subOrigData.R
@@ -1,7 +1,7 @@
 context("subOrigData")
 library(assignR)
 d1 = subOrigData(taxon = c("Danaus plexippus", "Setophaga ruticilla", "Turdus migratorius"))
-d2 = subOrigData(group = c("Passerine"), std_scale = "US_H_5")
+d2 = subOrigData(group = c("Passerine"), ref_scale = "US_H_5")
 
 test_that("suOrigData can correctly subset known-origin dataset in the assignR package",{
   expect_equal(class(d1), "subOrigData")
@@ -12,7 +12,7 @@ test_that("suOrigData can correctly subset known-origin dataset in the assignR p
   expect_warning(subOrigData(taxon = "Charadrius montanus", age_code = c("chick", "newborn")))
   expect_warning(subOrigData(taxon = c("Danaus plexippus", "Vanellus malabaricus")))
   expect_warning(subOrigData(group = c("Insect","Badgers")))
-  expect_warning(subOrigData(reference = c(3, "Ma 2020")))
-  expect_warning(subOrigData(reference = c(1, 100)))
+  expect_warning(subOrigData(dataset = c(5, "Ma 2020")))
+  expect_warning(subOrigData(dataset = c(5, 100)))
 })
   
