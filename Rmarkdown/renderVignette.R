4d47c0689c0f57bb62f24f064afa9734dfbe6068
Gabriel Bowen <gabe.bowen@utah.edu>
2022-02-24 11:29:15 -0700

    Update Rmd

--- Rmarkdown/renderVignette.R
+++ Rmarkdown/renderVignette.R
@@ -1,18 +1,15 @@
-renderVignette = function(){
-  dh = readLines("Rmarkdown/dev_header.Rmd")
-  ch = readLines("Rmarkdown/cran_header.Rmd")
-  b = readLines("Rmarkdown/body.Rmd")
-  
-  da = append(dh, b)
-  ca = append(ch, b)
-  
-  writeLines(da, "Rmarkdown/bound/assignR.Rmd")
-  writeLines(ca, "vignettes/assignR.Rmd")
-  
-  rmarkdown::render(
-    "Rmarkdown/bound/assignR.Rmd",
-    output_file = "../../docs/index.html"
-  )
-  
-  return(0)
-}
+
+dh = readLines("Rmarkdown/dev_header.Rmd")
+ch = readLines("Rmarkdown/cran_header.Rmd")
+b = readLines("Rmarkdown/body.Rmd")
+
+da = append(dh, b)
+ca = append(ch, b)
+
+writeLines(da, "Rmarkdown/bound/assignR.Rmd")
+writeLines(ca, "vignettes/assignR.Rmd")
+
+rmarkdown::render(
+  "Rmarkdown/bound/assignR.Rmd",
+  output_file = "../../docs/index.html"
+)
