1958064d4db23eb05de828abd78bd75abe2183e6
Gabriel Bowen <gabe.bowen@utah.edu>
2022-11-16 13:40:54 -0700

    Update getIsoscapes

--- tests/testthat/test_getIsoscapes.R
+++ tests/testthat/test_getIsoscapes.R
@@ -2,7 +2,7 @@ iso = getIsoscapes("CaribSr")
 iso2 = getIsoscapes("CaribSr")
 
 test_that("getIsoscapes works",{
-  expect_is(iso, "RasterStack")
+  expect_is(iso, "SpatRaster")
   expect_equal(iso, iso2)
   expect_error(getIsoscapes("USPrecipALLs"))
 })
