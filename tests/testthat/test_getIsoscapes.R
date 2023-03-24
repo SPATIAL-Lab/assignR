23f7caa32b66812eb3e0c6931e0329139d0315e9
Gabriel Bowen <gabe.bowen@utah.edu>
2023-03-24 14:06:10 -0600

    Finish merge into bybRgdal

--- tests/testthat/test_getIsoscapes.R
+++ tests/testthat/test_getIsoscapes.R
@@ -2,13 +2,9 @@ iso = getIsoscapes("CaribSr")
 iso2 = getIsoscapes("CaribSr")
 
 test_that("getIsoscapes works",{
-<<<<<<< HEAD
   if(!is.null(iso)){
     expect_is(iso, "SpatRaster")
   }
-=======
-  expect_is(iso, "SpatRaster")
->>>>>>> c37cfc69cc00426bb1adaa411491964227e16adb
   expect_equal(iso, iso2)
   expect_error(getIsoscapes("USPrecipALLs"))
 })
